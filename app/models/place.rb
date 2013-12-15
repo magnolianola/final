a56ff9ffba87a93b62588c4606d608a4c9cd3e51
nolalacey <nolalacey@hotmail.com>
2013-12-15 16:34:26 -0500

    added places

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,4 +2,5 @@ class Place < ActiveRecord::Base
 	validates :description, :name, :address, :phone, :email, :presence => true
 	geocoded_by :address
 	after_validation :geocode 
+	scope :closest_three, -> {limit(3)}
 end
